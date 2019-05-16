a99315f7f3046b25496f9a4d6cdb1fc66f876aa0
Andrey Semka <k9nz00@yandex.ru>
2019-05-16 16:15:47 +0300

    +

---
+++ authorization.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/includeFiles/template/header.php';
+?>
+<main class="page-authorization">
+  <h1 class="h h--1">Авторизация</h1>
+  <form class="custom-form" action="#" method="post">
+    <input type="email" class="custom-form__input" required="">
+    <input type="password" class="custom-form__input" required="">
+    <button class="button" type="submit">Войти в личный кабинет</button>
+  </form>
+</main>
+<?php
+require_once __DIR__.'/includeFiles/template/footer.php';
+?>

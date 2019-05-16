3fd71400ec16c16740d71b2d9c4e6b1055795937
Andrey Semka <k9nz00@yandex.ru>
2019-05-16 16:15:47 +0300

    +

--- add.php
+++ add.php
@@ -1,45 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="utf-8">
-  <title>Добавление товара</title>
+<?php
+require_once __DIR__.'/includeFiles/template/header.php';
+?>
 
-  <meta name="description" content="Fashion - интернет-магазин">
-  <meta name="keywords" content="Fashion, интернет-магазин, одежда, аксессуары">
-
-  <meta name="theme-color" content="#393939">
-
-  <link rel="preload" href="fonts/opensans-400-normal.woff2" as="font">
-  <link rel="preload" href="fonts/roboto-400-normal.woff2" as="font">
-  <link rel="preload" href="fonts/roboto-700-normal.woff2" as="font">
-
-  <link rel="icon" href="img/favicon.png">
-  <link rel="stylesheet" href="css/style.min.css">
-
-  <script src="js/scripts.js" defer=""></script>
-</head>
-<body>
-<header class="page-header">
-  <a class="page-header__logo" href="#">
-    <img src="img/logo.svg" alt="Fashion">
-  </a>
-  <nav class="page-header__menu">
-    <ul class="main-menu main-menu--header">
-      <li>
-        <a class="main-menu__item" href="index.html">Главная</a>
-      </li>
-      <li>
-        <a class="main-menu__item" href="products.html">Товары</a>
-      </li>
-      <li>
-        <a class="main-menu__item" href="orders.html">Заказы</a>
-      </li>
-      <li>
-        <a class="main-menu__item" href="#">Выйти</a>
-      </li>
-    </ul>
-  </nav>
-</header>
 <main class="page-add">
   <h1 class="h h--1">Добавление товара</h1>
   <form class="custom-form" action="#" method="post">
@@ -91,32 +53,6 @@
     </div>
   </section>
 </main>
-<footer class="page-footer">
-  <div class="container">
-    <a class="page-footer__logo" href="#">
-      <img src="img/logo--footer.svg" alt="Fashion">
-    </a>
-    <nav class="page-footer__menu">
-      <ul class="main-menu main-menu--footer">
-        <li>
-          <a class="main-menu__item" href="#">Главная</a>
-        </li>
-        <li>
-          <a class="main-menu__item" href="#">Новинки</a>
-        </li>
-        <li>
-          <a class="main-menu__item" href="index.html">Sale</a>
-        </li>
-        <li>
-          <a class="main-menu__item">Доставка</a>
-        </li>
-      </ul>
-    </nav>
-    <address class="page-footer__copyright">
-      © Все права защищены
-    </address>
-  </div>
-</footer>
-
-</body>
-</html>
+<?php
+require_once __DIR__.'/includeFiles/template/footer.php';
+?>
